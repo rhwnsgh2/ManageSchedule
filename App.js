5131fcb2b09511e5f0a2b35a8ae17c0eb7db20f4
고준호 <rhwnsgh2@naver.com>
2021-07-05 15:42:50 +0900

    init project

---
+++ App.js
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>mainPage</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
